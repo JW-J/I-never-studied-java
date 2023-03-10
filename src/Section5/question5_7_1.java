e88e011663fe5c977d8ee8e079eea17a3ae0e0b3
Juwon Jeong <62598523+JW-J@users.noreply.github.com>
2023-03-10 15:45:24 +0900

    question5_7_2 문제 푸는중.

--- src/Section5/question5_7_1.java
+++ src/Section5/question5_7_1.java
@@ -1,6 +1,6 @@
 package Section5;
 
-public class question5_7 {
+public class question5_7_1 {
 
     // 2,4,6,8단만 출력하고 2단은 2x2 , 4단은 4x4 이런식으로만 되도록 한다.
 // break 는 현재있는 반복문만 나갈 수 있지만 [output:] 을 사용하여 자신이 원하는 for문을 종료할 수 있다.
---
+++ src/Section5/question5_7_2.java
@@ -0,0 +1,16 @@
+package Section5;
+
+public class question5_7_2 {
+    // 다음 식을 만족하는 모든 A와 B의 조합을 구하는 프로그램을 작성해보기
+    // A B
+    // B A
+    //------
+    // 9 9
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+}
