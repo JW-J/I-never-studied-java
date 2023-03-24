a48e4ea1051cf1a5903ad1a4e911cacea023a2c2
Juwon Jeong <62598523+JW-J@users.noreply.github.com>
2023-03-24 15:11:36 +0900

    question6_2_2 문제 풀이 완료

---
+++ src/Section6/Question6_3_0.java
@@ -0,0 +1,13 @@
+package Section6;
+
+public class Question6_3_0 {
+    public static void main(String[] args) {
+        System.out.println("4의 재귀 : " + fast(4));   // 4 * 3 * 2 * 1
+    }
+    public static int fast(int num){
+        if(num == 1)
+            return 1;
+        else
+            return num * fast(num-1);
+    }
+}
---
+++ src/Section6/Question6_3_1.java
@@ -0,0 +1,19 @@
+package Section6;
+
+// 특정 인수와 제곱근을 지정하여 계산해주는 알고리즘.
+
+public class Question6_3_1 {
+    public static void main(String[] args) {
+
+        System.out.println(addfactorial(2, 5)); // num : 반복할 인수 ; count : 제곱근 지정
+
+    }
+    public static int addfactorial(int num, int count) {
+
+        if (count == 0) // 0의 제곱근을 할 경우 인수(예시:2)만 반환 한다.
+            return num; // count가 0인 경우 반복 인자를 반환해 곱해지도록 한다.
+        else
+            return num * addfactorial(num, count-1); // count-1을 통해 제곱은 횟수를 하나씩 차감한다.
+
+    }
+}
